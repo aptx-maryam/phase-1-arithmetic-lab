6abcb9b838de8c8cff178af8d5e2836404dfbf50
maryam haitham <marym.ihnj@gmail.com>
2022-08-08 23:19:37 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply)
+
+// function multiply(num1, num2){
+//     console.log(num1 * num2);
+// }
+// multiply(31, 2)
+
+const random = Math.floor(Math.random() * 100) + 10;
+console.log(random)
+
+// function random(){
+// console.log(Math.floor(Math.random() * 100) + 10);
+// }
+// random()
+
+ const num3 = 24;
+ const num4 = 5;
+ const mod = num3 % num4;
+ console.log(mod)
+
+// function mod (num3, num4){
+//     console.log(num3 % num4);
+// }
+// mod(24, 5)
+
+const max = Math.max(14, 5, 7, 10, 20)
+console.log(max)
+
+// function max(){
+//  console.log(Math.max(14, 5, 7, 10, 20));
+// }
+// max()
+
+
